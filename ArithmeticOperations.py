f9cefc8b2ee1d68a205f06a7c4f7c7f38dedf7a3
getmykhan <yusuf.khan2@studentpartner.com>
2016-05-29 21:12:21 +0530

    Arithmetic Operations like addition, subtraction, multiplication and division

---
+++ ArithmeticOperations.py
@@ -0,0 +1,25 @@
+"""
+Arithmetic Operations!
+"""
+
+def add(a, b):
+    sum1 = a + b
+    return sum1
+
+
+def sub(a, b):
+    sub1 = a - b
+    return sub1
+
+def mul(a, b):
+    mul1 = a * b
+    return mul1
+
+def div(a, b):
+    div1 = a // b
+    return div1
+
+print(add(20, 10))
+print(sub(20, 10))
+print(mul(20, 10))
+print(div(20, 10))
