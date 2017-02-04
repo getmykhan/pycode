104bf353ddd6a9506e6905f419270c87ae40fd6d
getmykhan <yusuf.khan2@studentpartner.com>
2017-02-04 14:48:36 -0500

    Basic Dictionary Creation

--- dictionary.py
+++ dictionary.py
@@ -8,9 +8,7 @@ dict0['Age'] = 22
 dict0['School'] = "NA"
 
 #print (dict0)
-
 #print (dict0['name'])
-
 
 for k , v in dict0.items():
     print (k)
@@ -19,6 +17,5 @@ for k , v in dict0.items():
 dict0['Age'] = 23
 #adding a new value with a key name 'age' #note: Age and age are two different key values and they are not the same.
 dict0['age'] = 22
-
 
 print(dict0['Age'], dict0['age'])
