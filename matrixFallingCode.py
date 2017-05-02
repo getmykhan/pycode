fe16b010bbc60655c0d258ac02dc0850a402c3f4
getmykhan <mkhan7@stevens.edu>
2017-05-02 11:57:35 -0400

    Matrix Code

---
+++ matrixFallingCode.py
@@ -0,0 +1,13 @@
+## Matrix like effect.
+## Author : getmykhan
+## Version : 1.0.0
+
+"""
+Really bored! So decided to create something old but interesting.
+Python Matrix Code Falling Effect.
+Enjoy!
+"""
+
+import random
+for i in range(1, 10000000):
+    print random.randint(1, 67182412871428486175732895732895937573258732367563246327426384628374628374683274326874628775698237582364326432678978798798778673526352743265)
