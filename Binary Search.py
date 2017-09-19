b77c6d579e0eb8f26b81ea1578d6c7dccf5c0f34
getmykhan <mkhan7@stevens.edu>
2017-09-18 20:25:35 -0400

    Minor tweaks

--- Binary Search.py
+++ Binary Search.py
@@ -1,34 +1,35 @@
 a = [7, 9, 10 , 13, 14, 17, 19,20]
 
-b = 17
+b = 9
 
 mid = len(a) //2
 
-if a[mid] < b:
-  print("true")
-  min_ = (len(a) - mid)
-  #print(min_)
+try:
+  if a[mid] < b:
+    print("true")
+    min_ = (len(a) - mid)
+    #print(min_)
 
-  for i in range(min_):
-    mid = mid + 1
-    if a[mid] == b:
-      print("Greater than side")
-      print("It is at index", mid)
-      break
+    for i in range(min_):
+      mid = mid + 1
+      if a[mid] == b:
+        print("Greater than side")
+        print("It is at index", mid)
+        break
 
-elif a[mid] == b:
-  print("Equal")
+  elif a[mid] == b:
+    print("Equal")
 
 
-elif a[mid] > b:
-  print("lola")
-  min_ = (len(a) - mid)
+  elif a[mid] > b:
+    print("lola")
+    min_ = (len(a) - mid)
 
-  for i in range (min_, 0, -1):
-    mid = mid - 1
-    if a[mid] == b:
-      print("Yes Less than")
-      break
+    for i in range (min_, 0, -1):
+      mid = mid - 1
+      if a[mid] == b:
+        print("Yes Less than")
+        break
 
-else:
+except:
   print("Element does not exist in the list")
